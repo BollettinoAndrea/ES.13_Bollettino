f107e0d2475b704f88fbef32a77cab217f5177de
bollettino.20159 <bollettino.andrea.studente@itispaleocapa.it>
2025-04-28 12:59:31 +0200

    commit_2 (Record.java)

--- src/Record.java
+++ src/Record.java
@@ -1,2 +1,37 @@
 public class Record {
+    private String regione;
+    private String anno;
+    private String valore;
+
+    public Record(String regione, String anno, String valore) {
+        this.regione = regione;
+        this.anno = anno;
+        this.valore = valore;
+    }
+
+    public String getRegione() {
+        return regione;
+    }
+    public void setRegione(String regione) {
+        this.regione = regione;
+    }
+
+    public String getAnno() {
+        return anno;
+    }
+    public void setAnno(String anno) {
+        this.anno = anno;
+    }
+
+    public String getValore() {
+        return valore;
+    }
+    public void setValore(String valore) {
+        this.valore = valore;
+    }
+
+    @Override
+    public String toString() {
+        return regione + ";" + anno + ";" + valore;
+    }
 }
